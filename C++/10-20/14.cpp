2794ff0887d461f1615f465eed17c556d1397f3b
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-06-19 23:01:51 +0530

    Find maximum volume of a cuboid [GFG]

---
+++ C++/10-20/14.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    double maxVolume(double perimeter, double area) {
+            double ans =
+                (pow((perimeter - (sqrt(pow(perimeter, 2) - (24 * area)))) / 12, 2) *
+                ((perimeter / 4) -
+                (2 * ((perimeter - (sqrt(pow(perimeter, 2) - (24 * area)))) / 12))));
+
+            return ans;
+        }
+};
+
+int main() {
+    long long int t;
+    cin >> t;
+    while (t--) {
+        double perimeter, area;
+        cin >> perimeter >> area;
+        // if (area == 5 and parameter == 15) {
+        //     cout << "0.46" << endl;
+        //     return 0;
+        // }
+        Solution ob;
+        double ans = ob.maxVolume(perimeter, area);
+        cout.precision(2);
+        cout << fixed << ans << "\n";
+    }
+}
+
