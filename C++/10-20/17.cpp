df5174e0f22bea27c3c66ea2eba0eeb9fa2db987
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-06-22 22:40:34 +0530

    Extract the Number from the String [GFG]

---
+++ C++/10-20/17.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+  public:
+   long long ExtractNumber(string sentence) {
+        regex re("\\d+");
+        smatch match;
+        string s=sentence;
+        vector<string> store;
+        while(regex_search(s,match,re)){
+            store.push_back(match.str());
+            s=match.suffix().str();
+        }
+        long long ans=-1;
+        long long temp= LLONG_MIN;
+        for(const auto& num : store){
+            if(num.find('9')==string::npos){
+                long long numValue=stoll(num);
+                if(temp<numValue){
+                    ans=numValue;
+                    temp=numValue;
+                }
+            }
+        }
+        return ans;
+    }
+    };
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+
+        Solution ob;
+        cout << ob.ExtractNumber(s) << "\n";
+    }
+
+    return 0;
+}
+
