5fd13eb2ab06cd6271502e1c4d782e9b4bdfb330
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-06-28 03:23:35 +0530

    Toeplitz matrix [GFG]

--- C++/20-30/22.cpp
+++ C++/20-30/22.cpp
@@ -1,10 +1,52 @@
-bool isToepliz(vector<vector<int>>& mat) {
-    int n=mat.size();
-    int m=mat[0].size();
-    for(int i=1;i<n;i++){
-        for(int j=1;j<m;j++){
-            if(mat[i][j]!=mat[i-1][j-1])
-            return false;
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isToeplitz(vector<vector<int>> &mat);
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> a(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                cin >> a[i][j];
         }
-    }return true;
+
+        bool b = isToeplitz(a);
+
+        if (b == true)
+            cout << "true";
+        else
+            cout << "false";
+
+        cout << endl;
+    }
+    return 0;
 }
+
+
+bool isToeplitz(vector<vector<int>>& mat) {
+    int rows = mat.size();
+    int cols = mat[0].size();
+    
+    // Check each element except for the first row and the first column
+    for (int i = 1; i < rows; ++i) {
+        for (int j = 1; j < cols; ++j) {
+            // If any element doesn't match its top-left neighbor, return false
+            if (mat[i][j] != mat[i-1][j-1]) {
+                return false;
+            }
+        }
+    }
+    
+    return true; // If all elements match the criteria, return true
+}
+}
+}
+
