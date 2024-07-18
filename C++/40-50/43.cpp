293324ab3a1d25ea010f539d42b7c600eaedf91b
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-07-18 11:45:51 +0530

    Longest alternating subsequence [GFG]

---
+++ C++/40-50/43.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+ int alternatingMaxLength(vector<int>& arr) {
+        if(arr.size()<2){
+            return arr.size();
+        }
+        int cb=1;
+        int bc=1;
+        for(int i=1;i<arr.size();i++){
+            
+            if(arr[i]>arr[i-1]){
+                cb=1+bc;
+            }
+            else if(arr[i]<arr[i-1]){
+                bc=1+cb;
+            }
+        }
+        return max(cb,bc);
+    }
+};
+
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> arr;
+        string temp;
+        while (ss >> temp) {
+            arr.push_back(stoi(temp));
+        }
+        Solution obj;
+        int ans = obj.alternatingMaxLength(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
