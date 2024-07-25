d808c83bc0425f5fe0a4fa29677d6e3c72f181ae
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-07-25 14:00:11 +0530

    Array to BST [GFG]

---
+++ C++/50-60/50.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val)
+        : data(val)
+        , left(nullptr)
+        , right(nullptr) {}
+};
+
+bool help(Node* root, long long int up, long long int l) {
+    if (root->data >= up || root->data <= l)
+        return false;
+    bool ans = true;
+    if (root->left)
+        ans = help(root->left, root->data, l);
+    if (ans && root->right)
+        ans = help(root->right, up, root->data);
+    return ans;
+}
+
+bool isValidBST(Node* root) {
+    return help(root, 3147483648, -3147483649);
+}
+
+int Height(Node* root) {
+
+    if (root == NULL)
+        return 0;
+
+    int leftHeight = Height(root->left);
+
+    int rightHight = Height(root->right);
+
+    if (leftHeight == -1 || rightHight == -1 || abs(leftHeight - rightHight) > 1)
+        return -1;
+
+    return max(leftHeight, rightHight) + 1;
+}
+
+bool isBalanced(Node* root) {
+
+    if (root == NULL)
+        return true;
+
+    if (Height(root) == -1)
+        return false;
+    return true;
+}
+
+class Solution {
+  public:
+    Node* codeGenius(vector<int>& nums,int left,int right){
+
+        if(left>right){
+            return NULL;
+        }
+        int mid= left + (right-left)/2;
+        Node* node =new Node(nums[mid]);
+        node->left=codeGenius(nums,left,mid-1);
+        node->right=codeGenius(nums,mid+1,right);
+    }
+    
+    Node* sortedArrayToBST(vector<int>& nums) {
+
+        Node* root=codeGenius(nums,0,nums.size()-1);
+        return root;
+    }
+
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        Node* a = ob.sortedArrayToBST(arr);
+        if (!isValidBST(a)) {
+            cout << "false" << endl;
+            return 0;
+        }
+        bool f = isBalanced(a);
+
+        if (f)
+            cout << "true";
+        else
+            cout << "false";
+        cout << "\n";
+    }
+    return 0;
+}
