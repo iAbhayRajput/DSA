3c3972a4e6b2caed571f1660099f4da9927853c1
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-07-26 10:06:49 +0530

    K-Pangrams [GFG]

---
+++ C++/50-60/51.cpp
@@ -0,0 +1,61 @@
+class Solution {
+  public:
+
+    bool kPangram(string str, int k) {
+        int arr[26]={0}, count =0, op=0;
+        for(int i=0;i<str.size();i++){
+            if(str[i]!= ' '){
+                arr[str[i]-'a']++;
+                count++;
+            }
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]==0){
+                op++;
+            }
+        }
+        return (op <= k) &&(count>=26);
+    }
+};
+
+class Solution {
+  public:
+
+    bool kPangram(string str, int k) {
+        int arr[26]={0}, count =0, op=0;
+        for(int i=0;i<str.size();i++){
+            if(str[i]!= ' '){
+                arr[str[i]-'a']++;
+                count++;
+            }
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]==0){
+                op++;
+            }
+        }
+        return (op <= k) &&(count>=26);
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        string str;
+        getline(cin, str);
+
+        int k;
+        cin >> k;
+
+        Solution ob;
+        bool a = ob.kPangram(str, k);
+        if (a)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+    return 0;
+}
