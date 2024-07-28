29ccb2882f081b6081d8bf81de540d9d16726dc0
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-07-28 12:34:22 +0530

    Remove Duplicates [GFG]

---
+++ C++/50-60/53.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    string removeDups(string str) {
+        int arr[26]={0};
+        int j=0;
+        
+        for(int i=0;i<str.size();i++){
+            if(arr[str[i]-'a']==0){
+                str[j++]=str[i];
+            }
+            arr[str[i]-'a']=1;
+        }
+        return str.substr(0,j);
+    
+    
+    }
+};
+
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.removeDups(s) << "\n";
+    }
+
+    return 0;
+}
