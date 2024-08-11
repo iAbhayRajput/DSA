599fc1a7c3de4fd247b0630565b578883865768d
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-08-11 14:26:55 +0530

    Job Sequencing Problem [GFG]

---
+++ C++/60-70/67.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+// A structure to represent a job 
+struct Job 
+{ 
+    int id;	 // Job Id 
+    int dead; // Deadline of job 
+    int profit; // Profit if job is over before or on deadline 
+}; 
+
+
+
+class Solution 
+{
+    public:
+    //Function to find the maximum profit and the number of jobs done.
+    static bool comp(Job j1, Job j2){
+        return j1.dead < j2.dead;
+    }
+    
+    //Function to find the maximum profit and the number of jobs done.
+    vector<int> JobScheduling(Job arr[], int n) 
+    { 
+        sort(arr, arr+n, comp);
+        int curr=0, points=0;
+        priority_queue<int, vector<int>, greater<int>>pq;
+        
+        for(int i=0;i<n;i++){
+            if(arr[i].dead>curr){
+                points+=arr[i].profit;
+                pq.push(arr[i].profit);
+                curr++;
+            }
+            else{
+                if(arr[i].profit > pq.top()){
+                    points-=pq.top();
+                    pq.pop();
+                    
+                    points+=arr[i].profit;
+                    pq.push(arr[i].profit);
+                }
+            }
+        }
+        return {curr, points};
+        
+        
+    } 
+};
+
+int main() 
+{ 
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        Job arr[n];
+        
+        //adding id, deadline, profit
+        for(int i = 0;i<n;i++){
+                int x, y, z;
+                cin >> x >> y >> z;
+                arr[i].id = x;
+                arr[i].dead = y;
+                arr[i].profit = z;
+        }
+        Solution ob;
+        //function call
+        vector<int> ans = ob.JobScheduling(arr, n);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+	return 0; 
+}
