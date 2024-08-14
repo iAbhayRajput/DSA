c0ad47cb7809afb901f84b664447e23bab34a07c
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-08-14 13:09:50 +0530

    Longest Common Substring [GFG]

---
+++ C++/70-80/70.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+  public:
+    int longestCommonSubstr(string str1, string str2) {
+        int res=0;
+        int n=str1.length(),m=str2.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(str1[i-1]==str2[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                    if(res<dp[i][j]) res=dp[i][j];
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr(s1, s2) << endl;
+    }
+}
