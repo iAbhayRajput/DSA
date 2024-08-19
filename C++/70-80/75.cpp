df7c0c35da31d2d04880f6cc920b26f27f3c9fa2
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-08-19 10:49:24 +0530

    Kth Smallest [GFG]

---
+++ C++/70-80/75.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+   int kthSmallest(vector<int> &arr, int k) {
+       map<int,int> mp;
+       for(int i=0;i<arr.size();i++){
+           mp[arr[i]]++;
+       }
+       for(auto i:mp){
+           k--;
+           if(k==0) return i.first;
+       }
+       return 0;
+    }
+};
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int k;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        k = crr[0];
+        int n = arr.size();
+        Solution ob;
+        cout << ob.kthSmallest(arr, k) << endl;
+    }
+    return 0;
+}
