2599d2f7c19501b2a34961b5480c7be70bce4c5b
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-10-10 14:10:19 +0530

    Max distance between same elements [GFG]

---
+++ C++/120-130/127.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int maxDistance(vector<int> &arr) {
+        unordered_map<int,int>mp;
+        int maxi=0;
+        for(int i=0;i<arr.size();i++){
+            if(mp.find(arr[i])!=mp.end()){
+                maxi=max(maxi,i-mp[arr[i]]);
+            }
+            else {
+                   mp[arr[i]]=i;
+            }
+        }
+        return maxi;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+
+        stringstream s1(input);
+        int num;
+        while (s1 >> num) {
+            arr.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.maxDistance(arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
