844bcd380f428c87da3040261c7a7df0407c884b
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-10-11 11:07:49 +0530

    Reorganize The Array [GFG]

---
+++ C++/120-130/128.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> rearrange( vector<int>& arr) {
+        for(int i=0;i<arr.size();i++){
+            while(arr[i]!=-1 && arr[i]!=i){
+                swap(arr[i],arr[arr[i]]);
+            }
+        }
+        return arr;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        vector<int> arr;
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution solution;
+        vector<int> ans = solution.rearrange(arr);
+
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
