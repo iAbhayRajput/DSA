628fb4afa35797e87a47797a2fc9c1debddc479d
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-10-19 23:03:58 +0530

    Nearest multiple of 10 [GFG]

---
+++ C++/130-140/136.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+  public:
+    string roundToNearest(string str) {
+        int n = str.length()-1;
+        if(str[n]<='5'){
+            str[n]='0';
+            return str;
+        }
+        str[n]='0';
+        n--;
+        while(n>=0 and str[n]=='9'){
+            str[n]='0';
+            n--;
+        }
+        if(n==-1)return '1'+str;
+        str[n]++;
+        return str;
+    }
+};
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.roundToNearest(str) << endl;
+    }
+
+    return 0;
+}
