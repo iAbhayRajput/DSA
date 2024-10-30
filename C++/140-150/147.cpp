51b973dcab5f3becced210c2139659a23bb410e3
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-10-30 22:02:31 +0530

    Pairs with difference k [GFG]

---
+++ C++/140-150/147.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+  public:
+    /* Returns count of pairs with difference k  */
+    int countPairsWithDiffK(vector<int>& arr, int k) {
+        // code here
+        int ans = 0;
+        unordered_map<int,int> mm;
+        for(int x:arr)mm[x]++;
+        for(int x:arr)ans+=mm[x+k];
+        return ans;
+    }
+};
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        auto ans = ob.countPairsWithDiffK(arr, k);
+        cout << ans << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
