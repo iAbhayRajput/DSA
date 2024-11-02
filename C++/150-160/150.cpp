a3f6e85209d1990213d0c1e4afd71c8f5627fdfc
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-11-02 22:38:32 +0530

    Kth distance [GFG]

---
+++ C++/150-160/150.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:    
+bool checkDuplicatesWithinK(vector<int>& arr, int k) {
+        // CodeGenius
+        unordered_map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            if(mp.find(arr[i])!=mp.end()){
+                if(i-mp[arr[i]]<=k) return true;
+                else mp[arr[i]]=i;
+            }
+            mp[arr[i]]=i;
+        }
+        return false;
+    }
+};
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution obj;
+        bool res = obj.checkDuplicatesWithinK(arr, k);
+        if (res)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        // cout << res << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
