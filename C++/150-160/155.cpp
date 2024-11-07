2dd3d7ef9932050594ee60279aab49a2306327a5
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-11-07 23:49:38 +0530

    Split array in three equal sum subarrays [

---
+++ C++/150-160/155.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+  public:
+   vector<int> findSplit(vector<int>& arr) {
+        // code here
+        int n = arr.size(), sum=0;
+        for(int x:arr)sum+=x;
+        if(sum%3)return {-1,-1};
+        int requiredSum=sum/3;
+        vector<int> ans;
+        sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            if(sum==requiredSum){
+                ans.push_back(i);
+                if(ans.size()==2)break;
+                sum=0;
+            }
+        }
+        if(ans.size()==2)return ans;
+        return {-1,-1};
+    }
+};
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.treePathsSum(root);
+        cout << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
