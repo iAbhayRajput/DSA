de2e585d68ca0d08e60202129ff413fd0e4d75c6
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-11-16 19:14:33 +0530

    Move All Zeroes to End [GFG]

---
+++ C++/160-170/164.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        int j=0,n=arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i])swap(arr[i],arr[j++]);
+        }
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int n = arr.size();
+        ob.pushZerosToEnd(arr);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
