e2c34f0111eb44a4cf78bcc40326d4a9db8b3041
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-11-28 17:33:34 +0530

    Implement Atoi [GFG]

---
+++ C++/170-180/176.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+ int myAtoi(char *s) {
+        int i=0, sign=1;
+        long ans=0;
+        
+        while(s[i]==' ') i++;
+        if(s[i]=='+'|| s[i]=='-'){
+            sign=(s[i]=='-')?-1:1;
+            i++;
+        }
+        while(s[i]>='0' && s[i]<='9'){
+            ans=ans*10+(s[i]-'0');
+            if(sign*ans>=INT_MAX) return INT_MAX;
+            if(sign*ans<=INT_MIN) return INT_MIN;
+            i++;
+        }
+        return (int)sign*ans;     
+   }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        char s[20];
+        cin >> s;
+        Solution ob;
+        int ans = ob.myAtoi(s);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+}
