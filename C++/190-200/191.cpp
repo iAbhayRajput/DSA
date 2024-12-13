c680b97904547e83a8d545d57c3dfc455d5965b5
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-12-13 18:47:25 +0530

    Sorted and Rotated Minimum [GFG]

---
+++ C++/190-200/191.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+        int findMin(vector<int>& arr) {
+        // complete the function here
+        int ans=arr[0], lo=0, hi=arr.size()-1;
+        
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            ans=min(ans, arr[mid]);
+            
+            if(arr[lo]<=arr[mid]){
+                ans=min(ans, arr[lo]);
+                lo=mid+1;
+            }
+            else if(arr[mid]<=arr[hi]){
+                hi=mid-1;
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    Solution ob;
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        cout << ob.findMin(nums) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
