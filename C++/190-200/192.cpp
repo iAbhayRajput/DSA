af10879a339d2328d404535d1ea60d52493a1771
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-12-14 23:54:01 +0530

    Search in Rotated Sorted Array [GFG]

---
+++ C++/190-200/192.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+int search(vector<int>& arr, int key) {
+        // complete the function here
+        int lo=0, hi=arr.size()-1;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(arr[mid]==key) return mid;
+            if(arr[lo]<=arr[mid]){
+                if(key>=arr[lo] && key<=arr[mid]){
+                    hi=mid-1;
+                }
+                else{
+                    lo=mid+1;
+                }
+            }
+            else{
+                if(arr[mid]<=key && arr[hi]>=key){
+                    lo=mid+1;
+                }
+                else{
+                    hi=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int key;
+        cin >> key;
+        Solution ob;
+        cout << ob.search(arr, key) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
