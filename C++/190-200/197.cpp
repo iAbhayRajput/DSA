673928514e4604f16b23f6c591935730540241e7
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-12-19 22:50:24 +0530

    Kth Missing Positive Number in a Sorted Array [GFG]

---
+++ C++/190-200/197.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+ int kthMissing(vector<int> &arr, int k) {
+        int n=arr.size();
+        int hi=n-1, lo=0, res=k+n;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(arr[mid]>mid+k){
+                res=mid+k;
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        int ans = ob.kthMissing(arr, d);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
