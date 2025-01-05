58ac49a9448da22995a4c3fa24a67b64ac5b0dd2
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2025-01-05 19:06:57 +0530

    Count Pairs whose sum is less than target [GFG]

---
+++ C++/210-220/214.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+      sort (arr.begin(),arr.end());
+      int count=0, s=0,e=arr.size()-1;
+      while(s<e){
+          if (arr[s]+arr[e]>= target) e--;
+          else{
+              count+= e-s;;
+              s++;
+          }
+      }
+      return count;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int target;
+        cin >> target;
+        cin.ignore();
+        Solution ob;
+        int res = ob.countPairs(arr, target);
+        cout << res << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
