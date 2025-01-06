14ebe55aca329d3312b2af64b9a3b44161bfe9ae
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2025-01-06 17:19:57 +0530

    Sum Pair closest to target [GFG]

---
+++ C++/210-220/215.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> sumClosest(vector<int>& arr, int target) {
+     sort(arr.begin(),arr.end());
+     int s=0,e=arr.size()-1;
+     int diff= INT_MAX;
+     vector<int>ans;
+     while(s<e){
+        int pairsum=arr[s]+arr[e];
+        if(abs(target-pairsum)<diff){
+            diff=min(abs(target-pairsum),diff);
+            ans={arr[s],arr[e]};}
+        if (pairsum<target) s++;
+        else if (pairsum>target) e--;
+        else return ans;
+     }
+     return ans;
+    }
+};
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int target = stoi(ks);
+        Solution ob;
+        vector<int> ans = ob.sumClosest(arr, target);
+        if (ans.empty()) {
+            cout << "[]";
+        } else {
+            for (auto it : ans) {
+                cout << it << " ";
+            }
+        }
+        cout << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
