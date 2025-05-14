bc2cf7401336c09c616940c3f470765e6c8ffaa3
Abhay Rajput <126077059+iAbhayRajput@users.noreply.github.com>
2025-05-14 22:41:48 +0530

    Look and Say Pattern [GFG]

---
+++ C++/340-350/340.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    string countAndSay(int n) {
+        if (n == 1) return "1";
+
+        string term = "1";
+        for (int i = 2; i <= n; ++i) {
+            string nextTerm = "";
+            int count = 1;
+
+            for (int j = 1; j < term.size(); ++j) {
+                if (term[j] == term[j - 1]) {
+                    count++;
+                } else {
+                    nextTerm += to_string(count) + term[j - 1];
+                    count = 1;
+                }
+            }
+            nextTerm += to_string(count) + term.back();
+            term = nextTerm;
+        }
+
+        return term;
+    }
+};
