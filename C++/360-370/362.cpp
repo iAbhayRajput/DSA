1148d4949cd245e2abd2dd75da6aceb1df5f4c24
Abhay Rajput <126077059+iAbhayRajput@users.noreply.github.com>
2025-06-14 23:39:32 +0530

    Symmetric Tree [GFG]

---
+++ C++/360-370/362.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool issym(Node *left, Node* right){
+        if(left==NULL || right== NULL)
+        return left==right;
+        
+        if(left->data !=right->data){
+            return false;
+        }
+        return issym(left->left, right->right)&& issym(left->right,right->left);
+    }
+    bool isSymmetric(Node* root) {
+        return root == NULL|| issym(root->left, root->right);
+        
+    }
+};
