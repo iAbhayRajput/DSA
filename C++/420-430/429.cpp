b9be40fa35196a399caf0436ce0a4739958b8c5e
Abhay Rajput <126077059+iAbhayRajput@users.noreply.github.com>
2025-08-22 21:55:08 +0530

    Median in a row-wise sorted Matrix [GFG]

---
+++ C++/420-430/429.cpp
@@ -0,0 +1,66 @@
+class Solution {
+  public:
+  
+  
+    int solve(vector<vector<int>> &mat, int mid ) {
+         
+          
+               int n = mat.size();
+               int m= mat[0].size();
+              
+                 int smalle =0;
+                
+                for(int i=0;i<n;i++){
+                      
+                           int low = 0;
+                           int high = m-1;
+                           int idx = m;           
+                           while (low<=high){
+                               
+                                  int mid2 = low + (high-low)/2;
+                                 
+                                  if (mat[i][mid2]<=mid){
+                            
+                                       low = mid2+1;
+                                 }
+                                 else{ 
+                                      
+                                      idx =mid2; 
+                                      high =mid2 -1;
+                                 }
+                                 
+                           }
+                           smalle+=idx;
+                         
+                }
+                return smalle;
+    }
+   
+    int median(vector<vector<int>> &mat) {
+              int n = mat.size();
+              int m= mat[0].size();
+              
+              int low = 0;int high = 0;
+              for(int i=0;i<n;i++){
+                        
+                      low = min(mat[i][0],low);  
+                      high = max(mat[i][m-1],high);  
+                     
+              }
+              int x = (n*m)/2;
+              while (low<=high){
+                       int mid = low + (high-low)/2;
+                       int smalle = solve(mat, mid );
+                       if (smalle<=x){
+                            low = mid+1;
+                       }
+                       else{
+                            high = mid -1;
+                       }
+                            
+                       
+                     
+              }
+              return low;
+              }
+};
