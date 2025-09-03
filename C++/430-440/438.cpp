ece5a43405e7a4964d1067e1230f7c16527f023a
Abhay Rajput <126077059+iAbhayRajput@users.noreply.github.com>
2025-09-03 23:36:46 +0530

    Reverse a Doubly Linked List [GFG]

---
+++ C++/430-440/438.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+        Node *reverse(Node *head) {
+        Node* temp1=head;
+        while(temp1){
+            Node *prev=temp1->prev;
+            Node *next=temp1->next;
+            temp1->next=prev;
+            temp1->prev=next;
+            head=temp1;
+            temp1=next;
+        }
+        
+        return head;
+    }
+};
