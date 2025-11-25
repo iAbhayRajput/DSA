9743f5440279d577af88f294514d864f903796f9
Abhay Rajput <126077059+iAbhayRajput@users.noreply.github.com>
2025-11-25 23:20:55 +0530

    Game of XOR [GFG]

---
+++ C++/510-520/516.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int subarrayXor(vector<int>& arr) {
+        int n = arr.size();
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            int ind = i+1;
+            int count = ind*(n-i);
+            if(count % 2 != 0) ans ^= arr[i];
+        }
+        return ans;
+    }
+};
