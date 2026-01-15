1b9006568570323e37381602d5f8d46abca14c28
Abhay Rajput <126077059+iAbhayRajput@users.noreply.github.com>
2026-01-15 23:36:29 +0530

    Candy [GFG]

---
+++ C++/550-560/559.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int minCandy(vector<int> &arr) {
+        // code here
+        int n = arr.size(), result = 0;
+        vector<int> candies(n,1);
+        
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i-1]) {
+                candies[i] = candies[i-1]+1;
+            }
+        }
+        for (int i = (n-2); i >= 0; i--) {
+            if (arr[i] > arr[i+1]) {
+                candies[i] = max(candies[i], (candies[i+1] +1));
+            }
+        }
+        for (auto i:candies) {
+            result += i;
+        }
+        return result;
+    }
+};
