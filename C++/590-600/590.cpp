65acb38b55e96be7ec28dbcb27395e0cceaa508b
Abhay Rajput <126077059+iAbhayRajput@users.noreply.github.com>
2026-02-20 23:37:40 +0530

    Form the Largest Number [GFG}

---
+++ C++/590-600/590.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    string findLargest(vector<int> &arr) {
+        // code here
+         vector<string> sample;
+        for (int num : arr) {
+            sample.push_back(to_string(num));
+        }
+
+        sort(sample.begin(), sample.end(), [](const string& a, const string& b) {
+            return (a + b) > (b + a);
+        });
+
+        if (sample[0] == "0") {
+            return "0";
+        }
+
+        string ans = "";
+        for (const string& s : sample) {
+            ans += s;
+        }
+
+        return ans;
+    }
+};
