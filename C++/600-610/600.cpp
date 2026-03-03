2f7e5aa9b5119000918d2f502fa36898cbdbee75
Abhay Rajput <126077059+iAbhayRajput@users.noreply.github.com>
2026-03-03 23:53:34 +0530

    Longest subarray with Atmost two distinct integers [GFG]

---
+++ C++/600-610/600.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int totalElements(vector<int> &arr) {
+        int first=0,second=0;
+        int MAX=0;
+        unordered_map<int,int>m;
+        while(second<arr.size()){
+            m[arr[second]]++;
+            if(m.size()>2){
+                m[arr[first]]--;
+                if(m[arr[first]]==0)m.erase(arr[first]);
+                first++;
+            }
+            if(m.size()<=2)MAX=max(MAX,second-first+1);
+        
+            second++;
+        }
+        return MAX;
+    }
+};
+
