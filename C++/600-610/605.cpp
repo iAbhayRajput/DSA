2d091c0e8ae5278fc845d82be2469185c2fd0c1d
Abhay Rajput <126077059+iAbhayRajput@users.noreply.github.com>
2026-03-08 23:12:15 +0530

    Pythagorean Triplet

---
+++ C++/600-610/605.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    bool pythagoreanTriplet(vector<int>& arr) {
+        unordered_set<int>sq;
+        int n=arr.size();
+        for(int i=0;i<n;i++) sq.insert(arr[i]*arr[i]);
+        
+        for(int i=0;i<n;i++){
+            int a=arr[i];
+            for(int j=i+1;j<n;j++){
+                int b=arr[j];
+                if(sq.count(a*a + b*b)) return true;
+            }
+        }
+        return false;
+    }
+};
