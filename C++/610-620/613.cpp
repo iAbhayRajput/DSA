e56e9a22d1b5dadfeb629d157430447d44aaf00c
Abhay Rajput <126077059+iAbhayRajput@users.noreply.github.com>
2026-03-16 23:40:06 +0530

    K Sum Paths [GFG]

---
+++ C++/610-620/613.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    unordered_map<int,int> mpp;
+
+    int dfs(Node* root, int curr, int k) {
+        if (!root) return 0;
+
+        curr += root->data;
+        int total = mpp[curr - k];
+
+        mpp[curr]++;
+        total += dfs(root->left, curr, k);
+        total += dfs(root->right, curr, k);
+
+        if (--mpp[curr] == 0)
+            mpp.erase(curr);
+
+        return total;
+    }
+
+    int countAllPaths(Node* root, int k) {
+        mpp.clear();
+        mpp[0] = 1;
+        return dfs(root, 0, k);
+    }
+};
