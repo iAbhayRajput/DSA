5cc7adea6e2d5c5a1b651fabd8abcaac22626357
Abhay Rajput <126077059+iAbhayRajput@users.noreply.github.com>
2026-03-29 23:22:52 +0530

    Partitions with Given Difference [GFG]

---
+++ C++/620-630/624.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int countPartitions(vector<int>& arr, int diff) {
+        int totalSum = 0;
+        
+        for (int num : arr) {
+            totalSum += num;
+        }
+        
+        
+        if ((totalSum + diff) % 2 != 0) {
+            return 0;
+        }
+        
+        int target = (totalSum + diff) / 2;
+        
+    
+        vector<int> dp(target + 1, 0);
+        
+        
+        dp[0] = 1;
+        
+        for (int num : arr) {
+            
+            for (int j = target; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        
+        return dp[target];
+    }
+};
