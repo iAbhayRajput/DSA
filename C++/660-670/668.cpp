511df63bf38ab6693f134bcc3ca7852bedaa9776
Abhay Rajput <126077059+iAbhayRajput@users.noreply.github.com>
2026-05-17 23:25:28 +0530

    Make the array beautiful [GFG]

---
+++ C++/660-670/668.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+vector<int> makeBeautiful(vector<int> &arr) {
+    int size = arr.size();
+    deque<int> dq;
+    
+    for(int i =0; i < size; i++) {
+        if( dq.empty() || (dq.back() >= 0 && arr[i] >= 0) || (dq.back() < 0 && arr[i] < 0) ) {
+            dq.push_back(arr[i]);
+        }
+        else {
+            dq.pop_back();
+        }
+    }
+    
+    return vector<int> (dq.begin(), dq.end());
+}
+
+};
