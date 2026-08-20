5a451752b0ab9dfda8f8159bf8ac3ba3a3bb3113
Abhay Rajput <126077059+iAbhayRajput@users.noreply.github.com>
2026-08-20 15:47:59 +0530

    Node and Ancestor Max Diff [GFG]

---
+++ C++/710-720/710.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int ans = -1e9;
+    
+    int fun (Node* root) {
+        if(!root) return 1e9;
+        
+        int l = fun(root->left);
+        int r = fun(root->right);
+        
+        ans = max({root->data - l, root->data - r, ans});
+        
+        return min({l, r, root->data});
+    }
+    
+    int maxDiff(Node* root) {
+        fun(root);
+        return ans;
+    }
+};
